f5cd36ebf3dd10f62e4626182ae438bd4d307650
longman14 <hassandamilola580@gmail.com>
2024-05-10 13:45:25 +0100

    First Commit

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT
